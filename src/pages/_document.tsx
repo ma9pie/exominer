518ab2e4637557c21d9836195c982c2626fea8c0
ma9pie <ma9pie.dev@gmail.com>
2025-03-09 22:39:16 +0900

    chore : _document 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <body className="antialiased">
         <Main />
